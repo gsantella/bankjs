7ebca70abfc2fac8f953e3b6d65422411712c9bc
gsantella <gsantella@southhills.edu>
2024-09-14 15:00:59 -0400

    filled out the classes

--- main.mjs
+++ main.mjs
@@ -2,9 +2,21 @@ import User from './models/user.mjs'
 import Account from './models/account.mjs'
 
 const user = new User()
+
+// interestingly these work
 const a1 = new Account()
 const a2 = new Account()
 
-user.accounts.push(a1, a2)
+// properly send values to constructor
+//but 500.25 could be "500.25" and could be "wiggles"
+const a3 = new Account(500.25, 'savings')
+
+
+//a3.deposit(44)
+//a3.withdrawal(44)
+
+user.accounts.push(a1, a2, a3)
 
 console.log(user)
+
+console.log(a3.balance)
--- models/account.mjs
+++ models/account.mjs
@@ -1,7 +1,37 @@
+import { v4 as uuidv4 } from 'uuid';
+
 export default class Account {
-    constructor() {
-        this.id = ""
-        this.balance = 0
-        this.type = ""
+    
+    // private instance fields
+    #id;
+    #balance;
+    #type;
+
+    // no overloaded constructor allowed in JavaScript
+    // constructor() {
+    //     this.id = ""
+    //     this.balance = 0
+    //     this.type = ""
+    // }
+
+    constructor(initialBalance, type) {
+        this.#id = uuidv4()
+        this.#balance = initialBalance
+        this.#type = type
+    }
+
+    get balance() {
+        //return this.balance throws a stack overflow!
+        return this.#balance
+    }
+
+    // doesn't say what it returns because it is a prototype based language
+    deposit(amount) {
+        throw 'Not Implemented'
+        //throw new NotImplementedException()
+    }
+
+    withdrawal(amount) {
+        throw new Exception('not implemented')
     }
 }
